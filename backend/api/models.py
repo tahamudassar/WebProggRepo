f99798eb0316cb72fd61c29b19e9645e642f2a4f
Munim <munim-k@users.noreply.github.com>
2026-04-05 01:39:17 +0500

    testing

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
+
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
